6025c8d4b61f8ab4bf7af8bd7ffabd78b99469dc
Hans3121 <hansyang2005@gmail.com>
2023-09-15 23:46:06 +0800

    Smooth Animation for progressBar

--- scripts/progress.js
+++ scripts/progress.js
@@ -6,6 +6,8 @@
  * Class to replace the functionality of a scrollbar by using a {@link progressBar}
  * to scroll/progress through multiple {@link screens}.
  * 
+ * Screens sizes should be equal to the parent container's size
+ * 
  * Animations should be done using css. This script adds the class 'show' 
  * when the screen is meant be showed and replaces it with the class 'hide' when it is meant to be hidden.
  * 
@@ -19,6 +21,12 @@ export class Progress {
     progressBar
 
     /**
+     * Animator for progress bar 
+     * @type {ProgressBarAnimator}
+     */
+    #progressBarAnimator
+
+    /**
      * HTMLDatalistElement for labelling the progress bar
      * @type {HTMLDataListElement}
      */
@@ -29,6 +37,12 @@ export class Progress {
      * @type {HTMLCollectionOf<Element>}
      */
     screens
+
+    /**
+     * parent container of screens
+     * @type {HTMLElement}
+     */
+    screenParent
 
     /**
      * The Index of the current screen
@@ -44,36 +58,32 @@ export class Progress {
 
     /**
      * This will determine the amount of value between screens in the progressBar.
-     * The max attribute will be {@link _rangeLengthMultiplier} * {@link screens}.length
+     * The max attribute will be {@link rangeLengthMultiplier} * {@link screens}.length
      * @type {number}
      */
-    _rangeLengthMultiplier = 1000
-    
-    /**
-     * This variable will hold the timeoutId of the SetTimeout that changes the value of the progressBar.
-     * Can be used to stop lerping the value of the progressBar
-     * @type {number}
-     */
-    _lerpTimeoutId
+    rangeLengthMultiplier = 1000
+
 
     /**
      * Construcs a progress object. Incomplete Documentation
      * @param {HTMLInputElement} progressBar
      * @param {HTMLCollectionOf<Element>} screens
+     * @param {HTMLElement} screenParent
      * @param {HTMLDataListElement} datalist
      */
     constructor(progressBar, screens, datalist) {
         this.progressBar = progressBar
+        this.#progressBarAnimator = new ProgressBarAnimator(progressBar) 
         this.screens = screens
         this.datalist = datalist
         this.currentScreenIndex = 0
-        this._setup()
+        this.#setup()
     }
 
     /**
-     * Private function to setup the progressbar, called when class is constructed.
+     * Private function to setup the whole thing, called when class is constructed.
      */
-    _setup() {
+    #setup() {
         // Sets up the screens list
         for (let i = 1; i < this.screens.length; i++){
             this.screens[i].classList.add("hide")
@@ -82,19 +92,34 @@ export class Progress {
         this.screens[0].classList.remove("hide")
         this.screens[0].classList.add("show")
 
+        this.#setupProgressBar()
+        this.#setupDatalist()
+        
+    }
+
+    #progressBarChangeTimeoutId
+    #setupProgressBar() {
         // Sets up the progress bar
-        this.progressBar.max = (this.screens.length - 1) * this._rangeLengthMultiplier
+        this.progressBar.max = (this.screens.length - 1) * this.rangeLengthMultiplier
         // this.progressBar.step = this._rangeLengthMultiplier
         this.progressBar.min = 0
         this.progressBar.value = 0
         this.progressBar.addEventListener('change', (ev) => {
-            this.changeScreen(this.progressBar.value / this._rangeLengthMultiplier)
-        })
+            this.#progressBarAnimator.stopAnim()
 
-        // Sets up the Datalist Options
+            clearTimeout(this.#progressBarChangeTimeoutId)
+            this.#progressBarChangeTimeoutId = setTimeout(()=>{
+                let index = Math.round(this.progressBar.value / this.rangeLengthMultiplier)
+                this.changeScreen(index)
+            },10)
+            
+        })
+    }
+
+    #setupDatalist() {
         for (let i = 0; i < this.screens.length; i++) {
             let option = document.createElement("option")
-            option.value = i * this._rangeLengthMultiplier
+            option.value = i * this.rangeLengthMultiplier
             option.label = this.screens[i].dataset.title || "Untitled"
             option.addEventListener("click", (ev) => {
                 this.changeScreen(i)
@@ -103,6 +128,8 @@ export class Progress {
         }
     }
 
+
+    _changeScreenTimeoutId
     /**
      * Changes the screen and moves the scrollbar by calling {@link scrollProgressBar}.
      * Index should be between 0 to the length of screens
@@ -110,44 +137,101 @@ export class Progress {
      */
     changeScreen(newScreenIndex) {
         // this.progressBar.value = newScreenIndex * this._rangeLengthMultiplier
-        this.scrollProgressBar(newScreenIndex * this._rangeLengthMultiplier)
+        this.#progressBarAnimator.scrollTo(newScreenIndex * this.rangeLengthMultiplier)
 
+        if (newScreenIndex == this.currentScreenIndex) {
+            return
+        }
 
+        
         // Hide Current Screen
         let currentScreen = this.screens[this.currentScreenIndex].classList
         currentScreen.remove("show")
         currentScreen.add("hide")
 
+        this.currentScreenIndex = newScreenIndex;
+
+        clearTimeout(this._changeScreenTimeoutId)
         // Show Current Screen after x minutes
+        // Simulates a closure.. not pretty though..
+        this._changeScreenTimeoutId = setTimeout(()=>{
+                let newScreen = this.screens[newScreenIndex].classList
+                newScreen.remove("hide")
+                newScreen.add("show")
+            }, this.changeScreenDelay)
+        
+
+    }
+}
+
+// Allow scrolling here?
+// Maybe, to seperate functionality
+class ProgressBarAnimator {
+    /**
+     * The Input Range bar to animate
+     * @type {HTMLInputElement}
+     */
+    _progressBar
+
+    /**
+     * An array containing the would be value of progress bar
+     * @type {Function[]}
+     */
+    _progressValueQueue
+
+    /**
+     * Ms of time between every setInterval 
+     */
+    _timeBetweenAnim = 10
+
+    /**
+     * Animation Manager for progressBar
+     * @param {HTMLInputElement} progressBar 
+     */
+    constructor(progressBar) {
+        this._progressBar = progressBar
+    }
+
+    scrollTo(newValue) {
+        this.stopAnim()
+        this._lerpFunc(parseInt(this._progressBar.value), newValue)
+        this.startAnim()
+    }
+
+    startAnim() {
         setTimeout(()=>{
-            let newScreen = this.screens[newScreenIndex].classList
-            newScreen.remove("hide")
-            newScreen.add("show")
+            if (this._progressValueQueue.length) {
+                this._progressValueQueue.pop()() // Execute the function
+                this.startAnim() // Loop until length runs out
+            }
+        }, 10)
+    }
 
-            this.currentScreenIndex = newScreenIndex 
-        }, this.changeScreenDelay)
-
+    stopAnim() {
+        this._progressValueQueue = []
     }
 
     /**
-     * Scrolls the {@link progressBar} to the value set in {@link newProgressBarValue}, which 
-     * should be the newScreenIndex * {@link _rangeLengthMultiplier}
-     * 
+     * @param {number} oldProgressBarValue 
      * @param {number} newProgressBarValue 
+     * @param {number} lerpValue 
      */
-    scrollProgressBar(newProgressBarValue) {
-        if (this._lerpTimeoutId){
-            clearTimeout(this._lerpTimeoutId)
+    _lerpFunc(oldProgressBarValue, newProgressBarValue, lerpValue = 0) {
+        lerpValue += 0.015 // Maybe could be customized
+
+        let lerpedProgressBarValue =  ProgressBarAnimator._lerp(
+            oldProgressBarValue,
+            newProgressBarValue,
+            ProgressBarAnimator._ease(lerpValue)
+        )
+
+        this._progressBar.value = lerpedProgressBarValue
+        
+        if (lerpValue < 1) {
+            this._progressValueQueue.push(()=>{
+                this._lerpFunc(oldProgressBarValue, newProgressBarValue, lerpValue)
+            })
         }
-
-        let oldProgressBarValue = parseInt(this.progressBar.value)
-        let lerpValue = 0
-
-        console.group(oldProgressBarValue, newProgressBarValue, lerpValue)
-
-        this._lerpTimeoutId = setTimeout(()=>{
-            this._lerpFunc(oldProgressBarValue, newProgressBarValue, lerpValue)
-        }, 1)
     }
 
     /**
@@ -162,7 +246,7 @@ export class Progress {
     }
 
     static _ease(x) {
-        x = Progress.clamp(0, x, 1)
+        x = ProgressBarAnimator.clamp(0, x, 1)
         return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
     }
 
@@ -176,40 +260,5 @@ export class Progress {
     static _lerp(a, b, value) {
         return a + value * (b-a)
     }
-
-    /**
-     * @param {number} oldProgressBarValue 
-     * @param {number} newProgressBarValue 
-     * @param {number} lerpValue 
-     */
-    _lerpFunc(oldProgressBarValue, newProgressBarValue, lerpValue) {
-        lerpValue += 0.015 // Maybe could be customized
-
-        let lerpedProgressBarValue =  Progress._lerp(
-            oldProgressBarValue,
-            newProgressBarValue,
-            Progress._ease(lerpValue)
-        )
-
-        this.progressBar.value = lerpedProgressBarValue
-        
-        if (lerpValue < 1) {
-            this._lerpTimeoutId = setTimeout(()=>{
-                this._lerpFunc(oldProgressBarValue, newProgressBarValue, lerpValue)
-            }, 10)
-        } else {
-            this._lerpTimeoutId = 0
-        }
-    }
-
-
-
-}
-
-// Allow scrolling here?
-// Maybe, to seperate functionality
-class ProgressBar {
-    constructor() {
-
-    }
+    
 }
