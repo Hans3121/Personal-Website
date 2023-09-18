efd859d4d402e023f075c514813b188590ecb2ec
Hans3121 <hansyang2005@gmail.com>
2023-09-18 22:48:54 +0800

    Refactoring Code
    Todo:
    - Style the buttons
    - Make it so that focusing on an element switches to that screen with node.contains(document.activeElement), i suppose.

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ let helloText = document.getElementById("hello-text")
 let hello = new HelloText(2000, 500, helloText)
 hello.start()
 
-let progressBar = document.getElementById("progress")
-let screens = document.getElementsByClassName("screen")
-let datalist = document.getElementById("screen-labels")
-let progress = new Progress(progressBar, screens, datalist)
+let progressSlider = document.getElementById("nav-slider") /* See CSS of nav-slider and children in index.css, since progress.js animates on it*/
+let screens = document.getElementById("screens") /* See CSS of screens and children in index.css, since progress.js animates on it */
+let buttonContainer = document.getElementsByTagName("nav")[0]
+let progress = new Progress(progressSlider, screens, buttonContainer)
 // progress.changeScreen(1)
--- scripts/progress.js
+++ scripts/progress.js
@@ -1,9 +1,5 @@
-// Todo: Add extra animation vs minimal animation, maybe by adding another class
-// Todo: Add scrollable feature, by padding the parent, possibly....
-
-
 /**
- * Class to replace the functionality of a scrollbar by using a {@link progressBar}
+ * Class to replace the functionality of a scrollbar by using a {@link progressSlider}
  * to scroll/progress through multiple {@link screens}.
  * 
  * Screens sizes should be equal to the parent container's size
@@ -16,39 +12,21 @@
 export class Progress {
     /** 
      * The Progress Bar of the website, replaces the scrollbar
-     * @type {HTMLInputElement}
+     * @type {HTMLElement}
      */
-    progressBar
-
-    /**
-     * Animator for progress bar 
-     * @type {ProgressBarAnimator}
-     */
-    #progressBarAnimator
-
-    /**
-     * HTMLDatalistElement for labelling the progress bar
-     * @type {HTMLDataListElement}
-     */
-    datalist
+    progressSlider
 
     /**
      * The screens the progress bar will progress through
-     * @type {HTMLCollectionOf<Element>}
+     * @type {HTMLCollectionOf<HTMLElement>}
      */
     screens
 
     /**
-     * parent container of screens
+     * Container for the buttons that will change the screens
      * @type {HTMLElement}
      */
-    screenParent
-
-    /**
-     * The Index of the current screen
-     * @type {number}
-     */
-    currentScreenIndex
+    buttonContainer
 
     /**
      * The MS delay of time when changing screens
@@ -56,27 +34,17 @@ export class Progress {
      */
     changeScreenDelay = 500
 
-    /**
-     * This will determine the amount of value between screens in the progressBar.
-     * The max attribute will be {@link rangeLengthMultiplier} * {@link screens}.length
-     * @type {number}
-     */
-    rangeLengthMultiplier = 1000
-
 
     /**
-     * Construcs a progress object. Incomplete Documentation
-     * @param {HTMLInputElement} progressBar
-     * @param {HTMLCollectionOf<Element>} screens
-     * @param {HTMLElement} screenParent
-     * @param {HTMLDataListElement} datalist
+     * Construcs a progress object. 
+     * @param {HTMLElement} progressSlider - The element that would be the progress slider
+     * @param {HTMLElement} screensContainer - Container of all the screens
+     * @param {HTMLElement} buttonContainer - Container that will be filled with buttons to change screens
      */
-    constructor(progressBar, screens, datalist) {
-        this.progressBar = progressBar
-        this.#progressBarAnimator = new ProgressBarAnimator(progressBar) 
-        this.screens = screens
-        this.datalist = datalist
-        this.currentScreenIndex = 0
+    constructor(progressSlider, screensContainer, buttonContainer) {
+        this.progressSlider = progressSlider
+        this.screens = screensContainer.children
+        this.buttonContainer = buttonContainer
         this.#setup()
     }
 
@@ -84,51 +52,24 @@ export class Progress {
      * Private function to setup the whole thing, called when class is constructed.
      */
     #setup() {
-        // Sets up the screens list
-        for (let i = 1; i < this.screens.length; i++){
-            this.screens[i].classList.add("hide")
-        }
-        // Sets up the first screen
-        this.screens[0].classList.remove("hide")
-        this.screens[0].classList.add("show")
-
-        this.#setupProgressBar()
-        this.#setupDatalist()
-        
-    }
-
-    #progressBarChangeTimeoutId
-    #setupProgressBar() {
-        // Sets up the progress bar
-        this.progressBar.max = (this.screens.length - 1) * this.rangeLengthMultiplier
-        // this.progressBar.step = this._rangeLengthMultiplier
-        this.progressBar.min = 0
-        this.progressBar.value = 0
-        this.progressBar.addEventListener('change', (ev) => {
-            this.#progressBarAnimator.stopAnim()
-
-            clearTimeout(this.#progressBarChangeTimeoutId)
-            this.#progressBarChangeTimeoutId = setTimeout(()=>{
-                let index = Math.round(this.progressBar.value / this.rangeLengthMultiplier)
-                this.changeScreen(index)
-            },10)
-            
-        })
-    }
-
-    #setupDatalist() {
+        // Setup the buttons
         for (let i = 0; i < this.screens.length; i++) {
-            let option = document.createElement("option")
-            option.value = i * this.rangeLengthMultiplier
-            option.label = this.screens[i].dataset.title || "Untitled"
-            option.addEventListener("click", (ev) => {
+            let btn = document.createElement("button")
+            btn.innerText = this.screens[i].dataset.title || "Untitled"
+            btn.addEventListener("click", () => {
                 this.changeScreen(i)
             })
-            this.datalist.appendChild(option)
+            this.buttonContainer.appendChild(btn)
         }
+
+        this.changeScreen(0)     
     }
 
 
+    /**
+     * Holds the Id of the Timeout Function so only one of it can exist at a time, 
+     * otherwise may cause weird screen changes
+     */
     _changeScreenTimeoutId
     /**
      * Changes the screen and moves the scrollbar by calling {@link scrollProgressBar}.
@@ -136,129 +77,23 @@ export class Progress {
      * @param {number} newScreenIndex 
      */
     changeScreen(newScreenIndex) {
-        // this.progressBar.value = newScreenIndex * this._rangeLengthMultiplier
-        this.#progressBarAnimator.scrollTo(newScreenIndex * this.rangeLengthMultiplier)
+        this.progressSlider.style.setProperty('--value', newScreenIndex * 100 / (this.screens.length-1) + "%")
 
-        if (newScreenIndex == this.currentScreenIndex) {
-            return
-        }
 
-        
-        // Hide Current Screen
-        let currentScreen = this.screens[this.currentScreenIndex].classList
-        currentScreen.remove("show")
-        currentScreen.add("hide")
-
-        this.currentScreenIndex = newScreenIndex;
+        this.#hideAllScreens()
 
         clearTimeout(this._changeScreenTimeoutId)
-        // Show Current Screen after x minutes
-        // Simulates a closure.. not pretty though..
         this._changeScreenTimeoutId = setTimeout(()=>{
-                let newScreen = this.screens[newScreenIndex].classList
-                newScreen.remove("hide")
-                newScreen.add("show")
+                this.#hideAllScreens()
+                this.screens[newScreenIndex].classList.replace("hide", "show")
             }, this.changeScreenDelay)
-        
-
-    }
-}
-
-// Allow scrolling here?
-// Maybe, to seperate functionality
-class ProgressBarAnimator {
-    /**
-     * The Input Range bar to animate
-     * @type {HTMLInputElement}
-     */
-    _progressBar
-
-    /**
-     * An array containing the would be value of progress bar
-     * @type {Function[]}
-     */
-    _progressValueQueue
-
-    /**
-     * Ms of time between every setInterval 
-     */
-    _timeBetweenAnim = 10
-
-    /**
-     * Animation Manager for progressBar
-     * @param {HTMLInputElement} progressBar 
-     */
-    constructor(progressBar) {
-        this._progressBar = progressBar
     }
 
-    scrollTo(newValue) {
-        this.stopAnim()
-        this._lerpFunc(parseInt(this._progressBar.value), newValue)
-        this.startAnim()
-    }
-
-    startAnim() {
-        setTimeout(()=>{
-            if (this._progressValueQueue.length) {
-                this._progressValueQueue.pop()() // Execute the function
-                this.startAnim() // Loop until length runs out
-            }
-        }, 10)
-    }
-
-    stopAnim() {
-        this._progressValueQueue = []
-    }
-
-    /**
-     * @param {number} oldProgressBarValue 
-     * @param {number} newProgressBarValue 
-     * @param {number} lerpValue 
-     */
-    _lerpFunc(oldProgressBarValue, newProgressBarValue, lerpValue = 0) {
-        lerpValue += 0.015 // Maybe could be customized
-
-        let lerpedProgressBarValue =  ProgressBarAnimator._lerp(
-            oldProgressBarValue,
-            newProgressBarValue,
-            ProgressBarAnimator._ease(lerpValue)
-        )
-
-        this._progressBar.value = lerpedProgressBarValue
-        
-        if (lerpValue < 1) {
-            this._progressValueQueue.push(()=>{
-                this._lerpFunc(oldProgressBarValue, newProgressBarValue, lerpValue)
-            })
+    #hideAllScreens() {
+        for (let i = 0; i < this.screens.length; i++){
+            let classList = this.screens[i].classList
+            classList.add("hide")
+            classList.remove("show")
         }
     }
-
-    /**
-     * Clamp
-     * @param {number} a - lower limit
-     * @param {number} val - value to be clamped
-     * @param {number} b - upper limit
-     * @returns {number}
-     */
-    static clamp(a, val, b) {
-        return Math.min(b, Math.max(val, a))
-    }
-
-    static _ease(x) {
-        x = ProgressBarAnimator.clamp(0, x, 1)
-        return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
-    }
-
-    /**
-     * Linear Interpolates between numbers. Use eased values to add easing or other stuff
-     * @param {number} a - Beginning value 
-     * @param {number} b - Ending value
-     * @param {number} value - Value to interpolate between. value isn't clamped.
-     * @returns {number} - Interpolated value
-     */
-    static _lerp(a, b, value) {
-        return a + value * (b-a)
-    }
-    
 }
